304f1e36e759782229719bac52b90c15f74e1c9c
COVID19 <yjgbg1239461052@gmail.com>
2020-12-07 00:04:15 +0800

    init

--- src/main/java/com/github/yjgbg/demo201202/ctl/HelloController.java
+++ src/main/java/com/github/yjgbg/demo201202/ctl/HelloController.java
@@ -1,14 +1,19 @@
 package com.github.yjgbg.demo201202.ctl;
 
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     private static int x = 0;
+
+    @SneakyThrows
     @GetMapping("hello")
     public String hello() {
-        System.out.println("hello,world");
+        Thread.sleep(200);
+        if (x%100 == 0) System.out.printf("访问了%s次数\n",x);
         return "hello,world"+x++;
     }
 }
